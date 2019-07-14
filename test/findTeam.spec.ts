34dc3279142e5950aa6752f7d66cea6ce1e75fb6
matt jennings <mattjennings44@gmail.com>
2019-07-14 01:35:23 -0600

    add tests for social media

--- test/findTeam.spec.ts
+++ test/findTeam.spec.ts
@@ -1,25 +1,30 @@
-import { findTeam } from '../src'
+import { teams, findTeam } from '../src'
 
 describe('findTeam', () => {
   describe('Abbreviations', () => {
     describe('Anaheim', () => {
       it('should return ANA', () => {
-        expect(findTeam('Ducks').abbreviation).toEqual('ANA')
+        expect(findTeam('Ducks')).toEqual(teams.ANA)
       })
     })
 
     describe('Boston', () => {
       it('should return BOS', () => {
-        expect(
-          findTeam('bruins', { caseSensitive: false }).abbreviation
-        ).toEqual('BOS')
+        expect(findTeam('bruins', { caseSensitive: false })).toEqual(teams.BOS)
       })
     })
 
     describe('Minnesota', () => {
       it('should return MIN', () => {
         const team = findTeam('mnwild', { caseSensitive: false })
-        expect(team.abbreviation).toEqual('MIN')
+        expect(team).toEqual(teams.MIN)
+      })
+    })
+
+    describe('Vancouver', () => {
+      it('should return VAN', () => {
+        const team = findTeam('VAN', { caseSensitive: false })
+        expect(team).toEqual(teams.VAN)
       })
     })
   })
@@ -27,15 +32,13 @@ describe('findTeam', () => {
   describe('City names', () => {
     describe('Chicago', () => {
       it('should return Chicago', () => {
-        expect(findTeam('Blackhawks').cityName).toEqual('Chicago')
+        expect(findTeam('Blackhawks')).toEqual(teams.CHI)
       })
     })
 
     describe('Dallas', () => {
       it('should return Dallas', () => {
-        expect(findTeam('stars', { caseSensitive: false }).cityName).toEqual(
-          'Dallas'
-        )
+        expect(findTeam('stars', { caseSensitive: false })).toEqual(teams.DAL)
       })
     })
   })
@@ -43,15 +46,28 @@ describe('findTeam', () => {
   describe('Team names', () => {
     describe('Columbus', () => {
       it('should return Blue Jackets', () => {
-        expect(findTeam('Columbus').teamName).toEqual('Blue Jackets')
+        expect(findTeam('Columbus')).toEqual(teams.CBJ)
       })
     })
 
     describe('Colorado', () => {
       it('should return Avalanche', () => {
-        expect(
-          findTeam('avalanche', { caseSensitive: false }).teamName
-        ).toEqual('Avalanche')
+        expect(findTeam('avalanche', { caseSensitive: false })).toEqual(
+          teams.COL
+        )
+      })
+    })
+  })
+
+  describe('Social Media', () => {
+    describe('hash tags', () => {
+      it('should return Florida Panthers', () => {
+        expect(findTeam('#FlaPanthers')).toEqual(teams.FLA)
+      })
+    })
+    describe('twitter', () => {
+      it('should return Florida Panthers', () => {
+        expect(findTeam('@FlaPanthers')).toEqual(teams.FLA)
       })
     })
   })
